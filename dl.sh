ede5c6d35200e2c8ee6be4a3bfe88cb7e7318b09
Daniel Whitford <whitford.dsw@gmail.com>
2021-02-13 18:41:05 +0000

    remove shell file

--- dl.sh
+++
@@ -1,6 +0,0 @@
-
-curl --compressed -s -H -g \
-    'content-type: application/json' \
-    'https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=overview&structure=%7B"areaName":"areaName","date":"date","newCasesBySpecimenDate":"newCasesByPublishDate"%7D' \
-    | jq \
-    # | less
